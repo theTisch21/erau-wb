4383742679bd00c91a4eb7f92921a0ad0fd5d4ee
theTisch21 <samtisch@live.com>
2022-11-04 01:54:22 +0000

    Aircraft database setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script lang="ts">
+	import { lookup, type Aircraft } from "$lib/aircraft"
+	import { writable } from "svelte/store"
+    let aircraft = writable("")
+    let plane: Aircraft | null = null
+    aircraft.subscribe(async a => {
+        console.log(a)
+        plane = await lookup(a)
+    })
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<input bind:value={$aircraft}>
+<p>{JSON.stringify(plane)}</p>
