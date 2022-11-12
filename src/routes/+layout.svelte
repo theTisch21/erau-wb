3ebd9016cfa0a5fd378d31792e52c81a6d142e97
Sam Tischaefer <samtisch@live.com>
2022-11-11 17:32:19 -0700

    Logo :D + header/footer

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import Header from '$lib/Header.svelte';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>Source code: <a href="https://github.com/theTisch21/erau-wb">Click here!</a></p>
+</footer>
+
+<style>
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 40px 0;
+		}
+	}
+</style>
