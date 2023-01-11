e6bf6a1989b6582a2fd2ae7c685acc0c3e5f1969
Sam Tischaefer <samtisch@live.com>
2023-01-10 22:25:14 -0700

    Formatting

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,26 +1,28 @@
-import { decodeMetar } from "$lib/Calculators/metar"
-import type { PageServerLoad } from "./$types"
+import { decodeMetar } from '$lib/Calculators/metar'
+import type { PageServerLoad } from './$types'
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load({fetch}:any) {
-    //Acquire metar from AWC
-    const result = await fetch("https://aviationweather.gov/metar/data?ids=kprc&format=raw&date=&hours=0")
-    const text: string = await result.text()
+export async function load({ fetch }: any) {
+	//Acquire metar from AWC
+	const result = await fetch(
+		'https://aviationweather.gov/metar/data?ids=kprc&format=raw&date=&hours=0'
+	)
+	const text: string = await result.text()
 
-    //Parse data
-    const regex = /<code>(.*)<\/code>/
-    const regexResult = text.match(regex)
-    if(regexResult == null) {
-        return {
-            metar: "Unable to load metar!"
-        }
-    }
-    const metar = regexResult[1]
-    //For testing, lets me see output in VSCode's console
-    decodeMetar(metar)
+	//Parse data
+	const regex = /<code>(.*)<\/code>/
+	const regexResult = text.match(regex)
+	if (regexResult == null) {
+		return {
+			metar: 'Unable to load metar!'
+		}
+	}
+	const metar = regexResult[1]
+	//For testing, lets me see output in VSCode's console
+	decodeMetar(metar)
 
-    //Return data
-    return {
-        metar: metar
-    }
+	//Return data
+	return {
+		metar: metar
+	}
 }
