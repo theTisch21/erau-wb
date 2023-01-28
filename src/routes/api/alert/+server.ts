a5213e0ff40150cd331394bde3c8c1121a49e1a3
Sam Tischaefer <samtisch@live.com>
2023-01-28 14:52:20 -0700

    Finish alert system and authorization

--- src/routes/api/alert/+server.ts
+++ src/routes/api/alert/+server.ts
@@ -1,18 +1,23 @@
-import { error, json, type RequestHandler } from '@sveltejs/kit';
-import { get, writable, type Writable } from 'svelte/store';
+import { json, type RequestHandler } from '@sveltejs/kit'
+import { get, writable, type Writable } from 'svelte/store'
+import { API_TOKEN } from '$env/static/private'
 export type UserAlert = {
-  alert: boolean,
-  alertText: string
+	alert: boolean
+	alertText: string
 }
 
-const alertStore: Writable<UserAlert> = writable({alert: false, alertText: "OK"})
+const alertStore: Writable<UserAlert> = writable({ alert: false, alertText: 'OK' })
 
 export const GET: RequestHandler = () => {
-  return json(get(alertStore))
+	return json(get(alertStore))
 }
 
-export const POST: RequestHandler = async ({request}) => {
-  const recievedAlert: UserAlert = await request.json()
-  alertStore.set(recievedAlert)
-  return new Response("OK", {status: 200})
+export const POST: RequestHandler = async ({ request }) => {
+	const token = request.headers.get('Authorization')?.split(' ')[1]
+	if (token != API_TOKEN) { //TODO DO NOT COMMIT
+		return new Response('UNAUTHORIZED', { status: 401 })
+	}
+	const recievedAlert: UserAlert = await request.json()
+	alertStore.set(recievedAlert)
+	return new Response('OK', { status: 200 })
 }
