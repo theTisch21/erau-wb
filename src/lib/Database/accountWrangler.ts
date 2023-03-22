1466e9c1e6a14412d38ca000a042a6017eb5a799
Sam Tischaefer <samtisch@live.com>
2023-03-21 23:54:04 -0700

    Improve functions

--- src/lib/Database/accountWrangler.ts
+++ src/lib/Database/accountWrangler.ts
@@ -1,6 +1,7 @@
 import { getDb } from "./db"
 import type { Collection } from "mongodb"
 const db = getDb()
+import crypto from 'crypto';
 
 //Types
 export type User = {
@@ -12,10 +13,20 @@ export type User = {
 
 const accountColl: Collection<User> = db.collection("Accounts")
 
-async function getUser(username: string): User {
+function hash(input: string): string {
+    return crypto.createHash('sha256').update(input).digest('hex');
+}
+
+async function getUser(username: string): Promise<User> {
     const user = await accountColl.findOne({username: username})
-    if (typeof user == "") { //TODO
-        throw new Error("Hecc")
+    if (user == null) {
+        throw new Error("Hecc") //TODO
     }
     return user
+}
+
+async function checkPassword(username: string, inputPassword: string): Promise<boolean> {
+    const user = getUser(username)
+    const inputHash = hash(inputPassword)
+    return (await user).passwordHash == inputHash
 }
