1d15f083053389d83378a3e2670a6aef56f6888f
theTisch21 <samtisch@live.com>
2023-04-21 22:02:48 +0000

    Start ui

---
+++ src/lib/Calculators/TimeFuelDistance.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { writable } from 'svelte/store'
+
+	let startAlt = writable('')
+	let endAlt = writable('')
+	let startTemp = writable('')
+	let endTemp = writable('')
+</script>
+
+<main>
+    <label for="tfd-startalt">Start altitude</label>
+	<input id="tfd-startalt" bind:value={startAlt} placeholder="Start altitude" class={$startAlt == "" ? "empty" : "success"}>
+	<label for="tfd-starttemp">Start temperature °C</label>
+	<input id="tfd-starttemp" bind:value={startTemp} placeholder="Start temperature °C" class={$startTemp == "" ? "empty" : "success"}>
+	<label for="tfd-endalt">End altitude</label>
+	<input id="tfd-endalt" bind:value={endAlt} placeholder="End altitude" class={$endAlt == "" ? "empty" : "success"}>
+	<label for="tfd-endtemp">End temperature °C</label>
+	<input id="tfd-endtemp" bind:value={endTemp} placeholder="End temperature °C" class={$endTemp == "" ? "empty" : "success"}>
+</main>
+
+<style>
+	.empty {
+		background-color: lime;
+	}
+	.success {
+		background-color: white;
+	}
+	input {
+		transition: all 0.5s;
+	}
+</style>
