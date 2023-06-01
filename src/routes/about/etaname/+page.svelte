769ca6d0bb958218168ddffa6614e19f09085cdd
theTisch21 <samtisch@live.com>
2023-06-01 17:08:05 +0000

    Lay the framework for registration

---
+++ src/routes/about/etaname/+page.svelte
@@ -0,0 +1,9 @@
+<main>
+    <h1>How to find your ETA name</h1>
+    <ol>
+        <li>Log in to ETA</li>
+        <li>Click on the ops view button <!--Photo--></li>
+        <li>Find a line where you are scheduled</li>
+        <li>Copy and paste your name from the line into the website</li>
+    </ol>
+</main>
