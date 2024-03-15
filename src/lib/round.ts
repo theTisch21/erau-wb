d29f0eeb394c07741ab6a7892a2d1c7aab66b6f4
theTisch21 <samtisch@live.com>
2024-03-15 06:00:53 +0000

    Add validation to rounding

--- src/lib/round.ts
+++ src/lib/round.ts
@@ -1,15 +1,17 @@
+import { z } from "zod" 
+const n = z.number({invalid_type_error: "WBXXXX Rounding function recieved invalid number"}).finite({message: "WBXXXX Rounding function recieved infinity"})
 // 10 = 1 decimal place (.1), 100 = 2 (.01), 0.1 = 10s place, 1 = whole number
 export const RoundingConstant = 100
 export function round(input: number, down = false): number {
 	if (down) {
-		return Math.floor(input * RoundingConstant) / RoundingConstant
+		return Math.floor(n.parse(input) * RoundingConstant) / RoundingConstant
 	}
-	return Math.ceil(input * RoundingConstant) / RoundingConstant
+	return Math.ceil(n.parse(input) * RoundingConstant) / RoundingConstant
 }
 
 export function roundToPrecision(input: number, RoundingConstant = 100, down = false) {
 	if (down) {
-		return Math.floor(input * RoundingConstant) / RoundingConstant
+		return Math.floor(n.parse(input) * RoundingConstant) / RoundingConstant
 	}
-	return Math.ceil(input * RoundingConstant) / RoundingConstant
+	return Math.ceil(n.parse(input) * RoundingConstant) / RoundingConstant
 }
