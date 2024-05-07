e3679d3b805c5d1712e6edf7701afa2f3afa252c
theTisch21 <samtisch@live.com>
2024-05-07 03:49:04 +0000

    Remove Zod and unused code

--- src/lib/round.ts
+++ src/lib/round.ts
@@ -1,22 +1,19 @@
-import { z } from 'zod'
-const n = z
-	.number({
-		required_error: 'WBXXXX Rounding function did not get a number',
-		invalid_type_error: 'WBXXXX Rounding function recieved invalid number'
-	})
-	.finite({ message: 'WBXXXX Rounding function recieved infinity' })
+import { WB } from './WBError'
+
 // 10 = 1 decimal place (.1), 100 = 2 (.01), 0.1 = 10s place, 1 = whole number
 export const RoundingConstant = 100
 export function round(input: number, down = false): number {
+	if (isNaN(input)) throw new WB(9999, 'Invalid number sent to rounding function')
 	if (down) {
-		return Math.floor(n.parse(input) * RoundingConstant) / RoundingConstant
+		return Math.floor(input * RoundingConstant) / RoundingConstant
 	}
-	return Math.ceil(n.parse(input) * RoundingConstant) / RoundingConstant
+	return Math.ceil(input * RoundingConstant) / RoundingConstant
 }
 
 export function roundToPrecision(input: number, RoundingConstant = 100, down = false) {
+	if (isNaN(input)) throw new WB(9999, 'Invalid number sent to rounding function')
 	if (down) {
-		return Math.floor(n.parse(input) * RoundingConstant) / RoundingConstant
+		return Math.floor(input * RoundingConstant) / RoundingConstant
 	}
-	return Math.ceil(n.parse(input) * RoundingConstant) / RoundingConstant
+	return Math.ceil(input * RoundingConstant) / RoundingConstant
 }
