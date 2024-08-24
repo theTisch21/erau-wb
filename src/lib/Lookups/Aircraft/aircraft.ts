30ad206eff9d4fb1d48eeb5b89542bcf4c034c19
Sam Tischaefer <samtisch@live.com>
2024-08-23 22:55:34 -0700

    Update aircraft data

--- src/lib/Lookups/Aircraft/aircraft.ts
+++ src/lib/Lookups/Aircraft/aircraft.ts
@@ -7,78 +7,496 @@ export type Aircraft = {
 }
 
 const list: Aircraft[] = [
-	{ name: 'E1', tailNumber: 'N31ER', weight: 1219, arm: 32.4627, moment: 39572 },
-	{ name: 'E2', tailNumber: 'N42ER', weight: 1200.04, arm: 31.25, moment: 37501.25 },
-	{ name: 'R- 1', tailNumber: 'N912MA', weight: 1325.72, arm: 12.4164, moment: 16460.72 },
-	{ name: 'R- 2', tailNumber: 'N602ER', weight: 1707.7, arm: 41.071, moment: 70137 },
-	{ name: 'R- 3', tailNumber: 'N603ER', weight: 1712.4, arm: 40.8164, moment: 69894 },
-	{ name: 'R- 4', tailNumber: 'N604ER', weight: 1707.7, arm: 41.071, moment: 70137 },
-	{ name: 'R- 5', tailNumber: 'N605ER', weight: 1707.7, arm: 41.071, moment: 70137 },
-	{ name: 'R- 6', tailNumber: 'N506ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R- 7', tailNumber: 'N507ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R- 8', tailNumber: 'N608ER', weight: 1707.7, arm: 41.071, moment: 70137 },
-	{ name: 'R- 9', tailNumber: 'N509ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-10', tailNumber: 'N610ER', weight: 1707.7, arm: 41.071, moment: 70137 },
-	{ name: 'R-11', tailNumber: 'N511ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-12', tailNumber: 'N712ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-13', tailNumber: 'N513ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-14', tailNumber: 'N614ER', weight: 1707.7, arm: 41.071, moment: 70137 },
-	{ name: 'R-15', tailNumber: 'N615ER', weight: 1707.7, arm: 41.071, moment: 70137 },
-	{ name: 'R-16', tailNumber: 'N516ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-17', tailNumber: 'N617ER', weight: 1713.4, arm: 41.1608, moment: 70525 },
-	{ name: 'R-18', tailNumber: 'N718ER', weight: 1713.4, arm: 41.3529, moment: 70854 },
-	{ name: 'R-19', tailNumber: 'N619ER', weight: 1714.8, arm: 41.6707, moment: 71457 },
-	{ name: 'R-20', tailNumber: 'N620ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-21', tailNumber: 'N721ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-22', tailNumber: 'N522ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-23', tailNumber: 'N623ER', weight: 1645, arm: 39.2298, moment: 64533 },
-	{ name: 'R-24', tailNumber: 'N724ER', weight: 1713.4, arm: 41.3529, moment: 70854 },
-	{ name: 'R-25', tailNumber: 'N625ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-26', tailNumber: 'N726ER', weight: 1726.8, arm: 41.7779, moment: 72142 },
-	{ name: 'R-27', tailNumber: 'N627ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-28', tailNumber: 'N728ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-29', tailNumber: 'N629ER', weight: 1711.4, arm: 41.1704, moment: 70459 },
-	{ name: 'R-30', tailNumber: 'N730ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-31', tailNumber: 'N531ER', weight: 1701.2, arm: 41.185, moment: 70064 },
-	{ name: 'R-38', tailNumber: 'N538ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-39', tailNumber: 'N639ER', weight: 1684.73, arm: 40.5176, moment: 68261.29 },
-	{ name: 'R-42', tailNumber: 'N542ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-50', tailNumber: 'N350ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-51', tailNumber: 'N651ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-53', tailNumber: 'N653ER', weight: 1723.7, arm: 41.5826, moment: 71676 },
-	{ name: 'R-54', tailNumber: 'N554ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-55', tailNumber: 'N655ER', weight: 1723.7, arm: 41.5826, moment: 71676 },
-	{ name: 'R-56', tailNumber: 'N656ER', weight: 1723.7, arm: 41.5826, moment: 71676 },
-	{ name: 'R-57', tailNumber: 'N657ER', weight: 1723.7, arm: 41.5826, moment: 71676 },
-	{ name: 'R-58', tailNumber: 'N658ER', weight: 1706.4, arm: 41.4762, moment: 70775 },
-	{ name: 'R-59', tailNumber: 'N659ER', weight: 1706.4, arm: 41.4762, moment: 70775 },
-	{ name: 'R-60', tailNumber: 'N660ER', weight: 1706.4, arm: 41.4762, moment: 70775 },
-	{ name: 'R-61', tailNumber: 'N661ER', weight: 1708.4, arm: 41.437, moment: 70791 },
-	{ name: 'R-62', tailNumber: 'N662ER', weight: 1706.4, arm: 41.4762, moment: 70775 },
-	{ name: 'R-63', tailNumber: 'N563ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-64', tailNumber: 'N664ER', weight: 1708.4, arm: 41.437, moment: 70791 },
-	{ name: 'R-65', tailNumber: 'N565ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-66', tailNumber: 'N566ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-67', tailNumber: 'N667ER', weight: 1708.4, arm: 41.437, moment: 70791 },
-	{ name: 'R-68', tailNumber: 'N668ER', weight: 1706.4, arm: 41.4762, moment: 70775 },
-	{ name: 'R-69', tailNumber: 'N669ER', weight: 1706.4, arm: 41.4762, moment: 70775 },
-	{ name: 'R-70', tailNumber: 'N670ER', weight: 1720.4, arm: 41.0654, moment: 70649 },
-	{ name: 'R-71', tailNumber: 'N671ER', weight: 1707.7, arm: 41.071, moment: 70137 },
-	{ name: 'R-72', tailNumber: 'N672ER', weight: 1707.7, arm: 41.071, moment: 70137 },
-	{ name: 'R-73', tailNumber: 'N673ER', weight: 1714.8, arm: 41.6707, moment: 71457 },
-	{ name: 'R-74', tailNumber: 'N674ER', weight: 1707.7, arm: 41.071, moment: 70137 },
-	{ name: 'R-75', tailNumber: 'N675ER', weight: 1707.7, arm: 41.071, moment: 70137 },
-	{ name: 'R-76', tailNumber: 'N676ER', weight: 1707.7, arm: 41.071, moment: 70137 },
-	{ name: 'R-77', tailNumber: 'N577ER', weight: 1714.7, arm: 41.3367, moment: 70880 },
-	{ name: 'R-78', tailNumber: 'N578ER', weight: 1651, arm: 39.4131, moment: 65071 },
-	{ name: 'R-79', tailNumber: 'N679ER', weight: 1707.7, arm: 41.071, moment: 70137 },
-	{ name: 'R-82', tailNumber: 'N4944S', weight: 1881.28, arm: 38.1025, moment: 71681.5 },
-	{ name: 'R-90', tailNumber: 'N790ER', weight: 3259.53, arm: 95.47, moment: 311187.33 },
-	{ name: 'R-91', tailNumber: 'N791ER', weight: 3251.818, arm: 95.47, moment: 310451.06 },
-	{ name: 'R-92', tailNumber: 'N792ER', weight: 3248.5, arm: 95.47, moment: 310134.3 },
-	{ name: 'R-97', tailNumber: 'N597ER', weight: 3269.82, arm: 95.5965, moment: 312583.43 },
-	{ name: 'R-98', tailNumber: 'N598ER', weight: 3249.98, arm: 95.3612, moment: 309921.96 },
-	{ name: 'R-99', tailNumber: 'N599ER', weight: 3274.23, arm: 95.5572, moment: 312876.17 }
+	{
+		name: 'R- 1',
+		tailNumber: 'N912MA',
+		weight: 1325.72,
+		arm: 12.4164,
+		moment: 16460.72
+	},
+	{
+		name: 'R- 2',
+		tailNumber: 'N602ER',
+		weight: 1707.7,
+		arm: 41.071,
+		moment: 70137
+	},
+	{
+		name: 'R- 3',
+		tailNumber: 'N603ER',
+		weight: 1712.4,
+		arm: 40.8164,
+		moment: 69894
+	},
+	{
+		name: 'R- 4',
+		tailNumber: 'N604ER',
+		weight: 1707.7,
+		arm: 41.071,
+		moment: 70137
+	},
+	{
+		name: 'R- 5',
+		tailNumber: 'N605ER',
+		weight: 1707.7,
+		arm: 41.071,
+		moment: 70137
+	},
+	{
+		name: 'R- 6',
+		tailNumber: 'N506ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R- 7',
+		tailNumber: 'N507ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R- 8',
+		tailNumber: 'N608ER',
+		weight: 1707.7,
+		arm: 41.071,
+		moment: 70137
+	},
+	{
+		name: 'R- 9',
+		tailNumber: 'N509ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-10',
+		tailNumber: 'N610ER',
+		weight: 1707.7,
+		arm: 41.071,
+		moment: 70137
+	},
+	{
+		name: 'R-11',
+		tailNumber: 'N511ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-12',
+		tailNumber: 'N712ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-13',
+		tailNumber: 'N513ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-14',
+		tailNumber: 'N614ER',
+		weight: 1707.7,
+		arm: 41.071,
+		moment: 70137
+	},
+	{
+		name: 'R-15',
+		tailNumber: 'N615ER',
+		weight: 1707.7,
+		arm: 41.071,
+		moment: 70137
+	},
+	{
+		name: 'R-16',
+		tailNumber: 'N516ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-17',
+		tailNumber: 'N617ER',
+		weight: 1713.4,
+		arm: 41.1608,
+		moment: 70525
+	},
+	{
+		name: 'R-18',
+		tailNumber: 'N718ER',
+		weight: 1713.4,
+		arm: 41.3529,
+		moment: 70854
+	},
+	{
+		name: 'R-19',
+		tailNumber: 'N619ER',
+		weight: 1714.8,
+		arm: 41.6707,
+		moment: 71457
+	},
+	{
+		name: 'R-20',
+		tailNumber: 'N620ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-21',
+		tailNumber: 'N721ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-22',
+		tailNumber: 'N522ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-23',
+		tailNumber: 'N623ER',
+		weight: 1645,
+		arm: 39.2298,
+		moment: 64533
+	},
+	{
+		name: 'R-24',
+		tailNumber: 'N724ER',
+		weight: 1713.4,
+		arm: 41.3529,
+		moment: 70854
+	},
+	{
+		name: 'R-25',
+		tailNumber: 'N625ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-26',
+		tailNumber: 'N726ER',
+		weight: 1726.8,
+		arm: 41.7779,
+		moment: 72142
+	},
+	{
+		name: 'R-27',
+		tailNumber: 'N627ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-28',
+		tailNumber: 'N728ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-29',
+		tailNumber: 'N629ER',
+		weight: 1711.4,
+		arm: 41.1704,
+		moment: 70459
+	},
+	{
+		name: 'R-30',
+		tailNumber: 'N730ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-31',
+		tailNumber: 'N531ER',
+		weight: 1701.2,
+		arm: 41.185,
+		moment: 70064
+	},
+	{
+		name: 'R-38',
+		tailNumber: 'N538ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-39',
+		tailNumber: 'N639ER',
+		weight: 1684.73,
+		arm: 40.5176,
+		moment: 68261.29
+	},
+	{
+		name: 'R-42',
+		tailNumber: 'N542ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-50',
+		tailNumber: 'N350ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-51',
+		tailNumber: 'N651ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-53',
+		tailNumber: 'N653ER',
+		weight: 1723.7,
+		arm: 41.5826,
+		moment: 71676
+	},
+	{
+		name: 'R-54',
+		tailNumber: 'N554ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-55',
+		tailNumber: 'N655ER',
+		weight: 1723.7,
+		arm: 41.5826,
+		moment: 71676
+	},
+	{
+		name: 'R-56',
+		tailNumber: 'N656ER',
+		weight: 1723.7,
+		arm: 41.5826,
+		moment: 71676
+	},
+	{
+		name: 'R-57',
+		tailNumber: 'N657ER',
+		weight: 1723.7,
+		arm: 41.5826,
+		moment: 71676
+	},
+	{
+		name: 'R-58',
+		tailNumber: 'N658ER',
+		weight: 1706.4,
+		arm: 41.4762,
+		moment: 70775
+	},
+	{
+		name: 'R-59',
+		tailNumber: 'N659ER',
+		weight: 1706.4,
+		arm: 41.4762,
+		moment: 70775
+	},
+	{
+		name: 'R-60',
+		tailNumber: 'N660ER',
+		weight: 1706.4,
+		arm: 41.4762,
+		moment: 70775
+	},
+	{
+		name: 'R-61',
+		tailNumber: 'N661ER',
+		weight: 1708.4,
+		arm: 41.437,
+		moment: 70791
+	},
+	{
+		name: 'R-62',
+		tailNumber: 'N662ER',
+		weight: 1706.4,
+		arm: 41.4762,
+		moment: 70775
+	},
+	{
+		name: 'R-63',
+		tailNumber: 'N563ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-64',
+		tailNumber: 'N664ER',
+		weight: 1708.4,
+		arm: 41.437,
+		moment: 70791
+	},
+	{
+		name: 'R-65',
+		tailNumber: 'N565ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-66',
+		tailNumber: 'N566ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-67',
+		tailNumber: 'N667ER',
+		weight: 1708.4,
+		arm: 41.437,
+		moment: 70791
+	},
+	{
+		name: 'R-68',
+		tailNumber: 'N668ER',
+		weight: 1706.4,
+		arm: 41.4762,
+		moment: 70775
+	},
+	{
+		name: 'R-69',
+		tailNumber: 'N669ER',
+		weight: 1706.4,
+		arm: 41.4762,
+		moment: 70775
+	},
+	{
+		name: 'R-70',
+		tailNumber: 'N670ER',
+		weight: 1720.4,
+		arm: 41.0654,
+		moment: 70649
+	},
+	{
+		name: 'R-71',
+		tailNumber: 'N671ER',
+		weight: 1707.7,
+		arm: 41.071,
+		moment: 70137
+	},
+	{
+		name: 'R-72',
+		tailNumber: 'N672ER',
+		weight: 1707.7,
+		arm: 41.071,
+		moment: 70137
+	},
+	{
+		name: 'R-73',
+		tailNumber: 'N673ER',
+		weight: 1714.8,
+		arm: 41.6707,
+		moment: 71457
+	},
+	{
+		name: 'R-74',
+		tailNumber: 'N674ER',
+		weight: 1707.7,
+		arm: 41.071,
+		moment: 70137
+	},
+	{
+		name: 'R-75',
+		tailNumber: 'N675ER',
+		weight: 1707.7,
+		arm: 41.071,
+		moment: 70137
+	},
+	{
+		name: 'R-76',
+		tailNumber: 'N676ER',
+		weight: 1707.7,
+		arm: 41.071,
+		moment: 70137
+	},
+	{
+		name: 'R-77',
+		tailNumber: 'N577ER',
+		weight: 1714.7,
+		arm: 41.3367,
+		moment: 70880
+	},
+	{
+		name: 'R-78',
+		tailNumber: 'N578ER',
+		weight: 1651,
+		arm: 39.4131,
+		moment: 65071
+	},
+	{
+		name: 'R-79',
+		tailNumber: 'N679ER',
+		weight: 1707.7,
+		arm: 41.071,
+		moment: 70137
+	},
+	{
+		name: 'R-82',
+		tailNumber: 'N4944S',
+		weight: 1881.28,
+		arm: 38.1025,
+		moment: 71681.5
+	},
+	{
+		name: 'R-90',
+		tailNumber: 'N790ER',
+		weight: 3259.53,
+		arm: 95.47,
+		moment: 311187.33
+	},
+	{
+		name: 'R-91',
+		tailNumber: 'N791ER',
+		weight: 3251.818,
+		arm: 95.47,
+		moment: 310451.06
+	},
+	{
+		name: 'R-92',
+		tailNumber: 'N792ER',
+		weight: 3248.5,
+		arm: 95.47,
+		moment: 310134.3
+	},
+	{
+		name: 'R-97',
+		tailNumber: 'N597ER',
+		weight: 3269.82,
+		arm: 95.5965,
+		moment: 312583.43
+	},
+	{
+		name: 'R-98',
+		tailNumber: 'N598ER',
+		weight: 3249.98,
+		arm: 95.3612,
+		moment: 309921.96
+	},
+	{
+		name: 'R-99',
+		tailNumber: 'N599ER',
+		weight: 3274.23,
+		arm: 95.5572,
+		moment: 312876.17
+	}
 ]
 
 const testList: Aircraft[] = [
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -395,9 +395,9 @@
 				data from ETA
 			</p>
 			<p>
-				<strong>Please note again:</strong> These numbers from ETA are up to date as of June 8 2024,
-				but may not be completely up to date. <br />Please double-check with the POH during your
-				preflight or check with the numbers on ETA to ensure accurate calculations.
+				<strong>Please note again:</strong> These numbers from ETA are up to date as of August 23
+				2024, but may not be completely up to date. <br />Please double-check with the POH during
+				your preflight or check with the numbers on ETA to ensure accurate calculations.
 			</p>
 			<p>
 				Notice something out of date? Send me an email: <a href="mailto:admin@erauwb.com"
