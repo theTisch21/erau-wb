e429d16c3cb12d7a1910bfe432e9070304c23e34
Sam Tischaefer <samtisch@live.com>
2026-07-13 18:22:07 -0700

    Update AI Disclosure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -383,8 +383,9 @@
 			</h2>
 			<p>Fill out the info below to calculate weight and balance for your aircraft!</p>
 			<p>
-				<strong>AI Disclosure:</strong> Generative AI has not been used in the creation of this site.
-				The core calculation logic will always be hand-made.
+				<strong>AI Disclosure:</strong> Generative AI was used to create server configuration files but
+				has not been used in the creation of the website itself. The core calculation logic will always
+				be hand-made.
 			</p>
 			<p>
 				<strong>DISCLAIMER:</strong> These calculations are provided as reference, with no guarantee
